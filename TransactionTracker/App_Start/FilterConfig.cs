fb57cc467c3e6dc53816f7e538418e4058578603
ZoltarPoD <tylermcconville@gmail.com>
2014-12-06 14:58:13 -0600

    Default ASP.NET MVC template

---
+++ TransactionTracker/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TransactionTracker
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TransactionTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TransactionTracker.MvcApplication" Language="C#" %>
